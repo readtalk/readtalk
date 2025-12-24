1d914ba600126d9f6624a52ca3697c8351c56597
READTalk <redtalk.app@hotmail.com>
2025-12-24 21:39:15 +0700

    sw.js

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('readtalk').then(cache => 
+  cache.addAll(['./', './index.html', './log.html'])
+)));
+self.addEventListener('fetch', e => e.respondWith(
+  caches.match(e.request).then(response => response || fetch(e.request))
+));
