764251cf132f513fe2548fbf4c01ea61ce3e9ae8
READTalk <redtalk.app@hotmail.com>
2026-06-10 17:17:58 +0700

    Update business name in settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,7 +2,7 @@ export default {
 	title: `READTalk Astro Theme | A Marketing Website Theme for Startups and Businesses`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
 	url: `https://odyssey-theme.sapling.dev`, // No trailing slash!
-	name: `READTalk Inc. under SOEPARNO ENTERPRISE Corp.`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `SOEPARNOTech Inc. under SOEPARNO ENTERPRISE Corp.`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
