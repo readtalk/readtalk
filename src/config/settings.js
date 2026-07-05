2776961f2cd9ca1950a3ef87211e5de3dd191624
READTalk <redtalk.app@hotmail.com>
2026-07-06 03:45:40 +0700

    Update URL and name in settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
 	title: `READTalk Astro Theme | A Marketing Website Theme for Startups and Businesses`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.dev`, // No trailing slash!
-	name: `SOEPARNOTech Inc. under SOEPARNO ENTERPRISE Corp.`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	url: `https://web-readtalk.pages.dev`, // No trailing slash!
+	name: `SOEPARNO ENTERPRISE Corp.`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
